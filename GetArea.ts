d11247f6ab08b6e91f80564a1befa1b62d5b1319
chrismatgit <christianmatabaro@gmail.com>
2020-12-04 12:59:59 +0300

    Implement the ISP and prevent the violation of the SoC principle

--- GetArea.ts
+++ GetArea.ts
@@ -1,26 +1,49 @@
+interface RectangleInterface {
+  width: number;
+  height: number;
+}
+
+interface CircleInterface {
+  radius: number;
+}
+
 interface Shape {
   area(): number;
+}
+
+interface Serializable {
   serialize(): string;
 }
 
-class Rectangle implements Shape {
+class Rectangle implements RectangleInterface, Shape {
   public width: number;
   public height: number;
 
   public area() {
     return this.width * this.height;
   }
-  public serialize() {
-    return JSON.stringify(this);
-  }
 }
 
-class Circle implements Shape {
+class Circle implements CircleInterface, Shape {
   public radius: number;
 
   public area() {
     return this.radius * this.radius * Math.PI;
   }
+}
+
+class RectangleDTO implements RectangleInterface, Serializable {
+  public width: number;
+  public height: number;
+
+  public serialize() {
+    return JSON.stringify(this);
+  }
+}
+
+class CircleDTO implements CircleInterface, Serializable {
+  public radius: number;
+
   public serialize() {
     return JSON.stringify(this);
   }
